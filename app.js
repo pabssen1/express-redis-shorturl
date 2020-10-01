cf8b9d6289aeb25e93e2228b4baa92ad0ee95461
Debaditya Sen <debaditya_sen@srmuniv.edu.in>
2020-10-01 05:35:33 +0530

    Added URL update endpoint

--- app.js
+++ app.js
@@ -38,8 +38,26 @@ app.get("/:id", function (req, res, next) {
   });
 });
 
-app.post("/shorten", function (req, res, next) {
+app.post("/url/add", function (req, res, next) {
   let id = nanoid.nanoid(6);
+  let main_url = req.body.main_url;
+  main_url = addhttp(main_url);
+  client.hmset(id, ["main_url", main_url], function (err, reply) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send({
+        shortId: id,
+        redirectUrl: main_url
+      });
+    }
+    console.log(reply);
+    console.log(id);
+  });
+});
+
+app.post("/url/update", function (req, res, next) {
+  let id = req.body.id;
   let main_url = req.body.main_url;
   main_url = addhttp(main_url);
   client.hmset(id, ["main_url", main_url], function (err, reply) {
